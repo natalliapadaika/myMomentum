e3f8efc12d652632f8fca067c9acb339530f197d
Natallia <139593186+natalliapadaika@users.noreply.github.com>
2023-11-13 01:15:46 +0300

    Update play-list.js

--- script/play-list.js
+++ script/play-list.js
@@ -6,17 +6,17 @@ const playList = [
     },
     {
       title: 'River Flows In You',
-      src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/River Flows In You.mp3',
+      src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/River%20Flows%20In%20You.mp3',
       duration: '01:38'
     },
     {
         title: 'Ennio Morricone',
-        src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/Ennio Morricone.mp3',
+        src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/Ennio%20Morricone.mp3',
         duration: '01:38'
     },
     {
         title: 'Summer Wind',
-        src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/Summer Wind.mp3',
+        src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/Summer%20Wind.mp3',
         duration: '01:51'
 
     }
