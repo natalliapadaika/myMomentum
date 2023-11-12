b1a0503d9a27363b169ba68efe228b1e5d229d34
Nattallia <139593186+natalliapadaika@users.noreply.github.com>
2023-11-12 22:33:58 +0300

    Add files via upload

---
+++ script/play-list.js
@@ -0,0 +1,27 @@
+const playList = [
+    {
+      title: 'Aqua Caelestis',
+      src: '../sounds/Aqua Caelestis.mp3',
+      duration: '00:58'
+    },
+    {
+      title: 'River Flows In You',
+      src: '../sounds/River Flows In You.mp3',
+      duration: '01:38'
+    },
+    {
+        title: 'Ennio Morricone',
+        src: '../sounds/Ennio Morricone.mp3',
+        duration: '01:38'
+    },
+    {
+        title: 'Summer Wind',
+        src: '../sounds/Summer Wind.mp3',
+        duration: '01:51'
+
+    }
+  ]
+  export default playList;
+
+
+  
