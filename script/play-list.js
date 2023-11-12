e15419793285fb5e9b1b95f609be08e2b4378613
Natallia <139593186+natalliapadaika@users.noreply.github.com>
2023-11-13 01:30:36 +0300

    Add files via upload

--- script/play-list.js
+++ script/play-list.js
@@ -1,22 +1,22 @@
 const playList = [
     {
       title: 'Aqua Caelestis',
-      src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/Aqua%20Caelestis.mp3',
+      src: '../sounds/AquaCaelestis.mp3',
       duration: '00:58'
     },
     {
       title: 'River Flows In You',
-      src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/River%20Flows%20In%20You.mp3',
+      src: '../sounds/RiverFlowsInYou.mp3',
       duration: '01:38'
     },
     {
         title: 'Ennio Morricone',
-        src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/Ennio%20Morricone.mp3',
+        src: '../sounds/EnnioMorricone.mp3',
         duration: '01:38'
     },
     {
         title: 'Summer Wind',
-        src: 'https://github.com/natalliapadaika/myMomentum/blob/main/sounds/Summer%20Wind.mp3',
+        src: '../sounds/SummerWind.mp3',
         duration: '01:51'
 
     }
